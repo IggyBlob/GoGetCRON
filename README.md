8fea189cc48208edc38973a048a173ffc8bd8c2d
IggyBlob <socialmedia@paulhaunschmied.com>
2017-03-04 22:58:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoGetCRON
+
+A lightweight service that executes HTTP GET requests repeatedly at a given interval.
---
+++ main.go
@@ -0,0 +1,48 @@
+// GoGetCRON is a simple app that repeatedly executes HTTP requests
+package main
+
+import (
+"time"
+"net/http"
+"log"
+"errors"
+)
+
+const sleep = 2	// sleep times in hrs
+
+var urls = []string {
+	"http://www.paulhaunschmied.com/",
+	// ...
+}
+
+func main() {
+	log.Println("=== RadioCheckerCRON started ===")
+
+	ticker := time.NewTicker(1 * time.Minute)
+	for {
+		select {
+		case <- ticker.C:
+			for _, url := range urls {
+				if err := getHTTP(url); err != nil {
+					log.Printf("ERR %s\n", err.Error())
+					continue
+				}
+				log.Println("OK  " + url)
+			}
+
+		}
+	}
+}
+
+func getHTTP(url string) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return errors.New("response code " + string(resp.StatusCode) + " for " + url)
+	}
+	return nil
+}
